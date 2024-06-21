2b3198776b944ef88e4d2a8808bb3df63da611a2
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-06-21 19:54:51 +0100

    firt publishing plugin working

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm") version "1.9.23"
     id("java-library")
-    id("maven-publish")
+//    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.28.0"
     signing
 }
 
@@ -11,77 +14,109 @@ val ossrhPassword: String by project
 
 
 group = "io.github.vikkio88"
-//archivesBaseName = "kartazze"
-version = "0.0.2"
+version = "0.0.1alpha"
 val archiveName = "kartazze"
 
 repositories {
     mavenCentral()
 }
+mavenPublishing {
+    coordinates(
+        groupId = group as String,
+        artifactId = archiveName,
+        version = version as String
+    )
+    pom {
+        name.set("Kartazze")
+        description.set("A little ORM/Query Builder Library")
+        url.set("https://github.com/vikkio88/kartazze")
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifactId = archiveName
-            pom {
-                name.set("Kartazze")
-                description.set("A little ORM/Query Builder Library")
-                url.set("https://github.com/vikkio88/kartazze")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("vikkio88")
-                        name.set("Vincenzo Ciaccio")
-                        email.set("vincenzo.ciaccio@gmail.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/vikkio88/kartazze.git")
-                    developerConnection.set("scm:git:ssh://github.com/vikkio88/kartazze.git")
-                    url.set("https://github.com/vikkio88/kartazze")
-                }
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+
+        developers {
+            developer {
+                id.set("vikkio88")
+                name.set("Vincenzo Ciaccio")
+                email.set("vincenzo.ciaccio@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/vikkio88/kartazze.git")
+            developerConnection.set("scm:git:ssh://github.com/vikkio88/kartazze.git")
+            url.set("https://github.com/vikkio88/kartazze")
         }
     }
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-    repositories {
-        maven {
-            name = "ossrh"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                HttpHeaderAuthentication { "Bearer $ossrhToken" }
-                username = ossrhUsername
-                password = ossrhPassword
-            }
-        }
-
-
-        maven {
-            name = "ossrhSnapshots"
-            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            credentials {
-                HttpHeaderAuthentication { "Bearer $ossrhToken" }
-                username = ossrhUsername
-                password = ossrhPassword
-            }
-        }
-    }
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
 
-signing {
-    sign(publishing.publications["mavenJava"])
-}
 
-// Define the ossrhUsername and ossrhPassword somewhere in your script or in gradle.properties
+//publishing {
+//    publications {
+//        create<MavenPublication>("mavenJava") {
+//            from(components["java"])
+//            artifactId = archiveName
+//            pom {
+//                name.set("Kartazze")
+//                description.set("A little ORM/Query Builder Library")
+//                url.set("https://github.com/vikkio88/kartazze")
+//
+//                licenses {
+//                    license {
+//                        name.set("The Apache License, Version 2.0")
+//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                    }
+//                }
+//
+//                developers {
+//                    developer {
+//                        id.set("vikkio88")
+//                        name.set("Vincenzo Ciaccio")
+//                        email.set("vincenzo.ciaccio@gmail.com")
+//                    }
+//                }
+//
+//                scm {
+//                    connection.set("scm:git:git://github.com/vikkio88/kartazze.git")
+//                    developerConnection.set("scm:git:ssh://github.com/vikkio88/kartazze.git")
+//                    url.set("https://github.com/vikkio88/kartazze")
+//                }
+//            }
+//        }
+//    }
+//
+//    repositories {
+//        maven {
+//            name = "ossrh"
+//            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+//            credentials {
+//                HttpHeaderAuthentication { "Bearer $ossrhToken" }
+//                username = ossrhUsername
+//                password = ossrhPassword
+//            }
+//        }
+//
+//
+//        maven {
+//            name = "ossrhSnapshots"
+//            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+//            credentials {
+//                HttpHeaderAuthentication { "Bearer $ossrhToken" }
+//                username = ossrhUsername
+//                password = ossrhPassword
+//            }
+//        }
+//    }
+//}
 
 
 dependencies {
