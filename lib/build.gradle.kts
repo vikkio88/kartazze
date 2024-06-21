f1e5d33a8c0e6ba50160f16140d9c4991f1e284f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2024-06-21 20:26:14 +0100

    cleaned up gradle

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,9 +1,9 @@
+import com.vanniktech.maven.publish.KotlinJvm
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     kotlin("jvm") version "1.9.23"
     id("java-library")
-//    id("maven-publish")
     id("com.vanniktech.maven.publish") version "0.28.0"
     signing
 }
@@ -14,13 +14,18 @@ val ossrhPassword: String by project
 
 
 group = "io.github.vikkio88"
-version = "0.0.1alpha"
+version = "0.0.1alpha1"
 val archiveName = "kartazze"
 
 repositories {
     mavenCentral()
 }
 mavenPublishing {
+    configure(
+        KotlinJvm(
+            sourcesJar = true
+        )
+    )
     coordinates(
         groupId = group as String,
         artifactId = archiveName,
@@ -58,66 +63,6 @@ mavenPublishing {
     // Enable GPG signing for all publications
     signAllPublications()
 }
-
-
-//publishing {
-//    publications {
-//        create<MavenPublication>("mavenJava") {
-//            from(components["java"])
-//            artifactId = archiveName
-//            pom {
-//                name.set("Kartazze")
-//                description.set("A little ORM/Query Builder Library")
-//                url.set("https://github.com/vikkio88/kartazze")
-//
-//                licenses {
-//                    license {
-//                        name.set("The Apache License, Version 2.0")
-//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                    }
-//                }
-//
-//                developers {
-//                    developer {
-//                        id.set("vikkio88")
-//                        name.set("Vincenzo Ciaccio")
-//                        email.set("vincenzo.ciaccio@gmail.com")
-//                    }
-//                }
-//
-//                scm {
-//                    connection.set("scm:git:git://github.com/vikkio88/kartazze.git")
-//                    developerConnection.set("scm:git:ssh://github.com/vikkio88/kartazze.git")
-//                    url.set("https://github.com/vikkio88/kartazze")
-//                }
-//            }
-//        }
-//    }
-//
-//    repositories {
-//        maven {
-//            name = "ossrh"
-//            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-//            credentials {
-//                HttpHeaderAuthentication { "Bearer $ossrhToken" }
-//                username = ossrhUsername
-//                password = ossrhPassword
-//            }
-//        }
-//
-//
-//        maven {
-//            name = "ossrhSnapshots"
-//            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-//            credentials {
-//                HttpHeaderAuthentication { "Bearer $ossrhToken" }
-//                username = ossrhUsername
-//                password = ossrhPassword
-//            }
-//        }
-//    }
-//}
-
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
